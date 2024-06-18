9f675512d47171ac759a8ff565dc3bba66c3a16e
Kevelyn Azevedo lima <ra24305737841@cursos.insf>
2024-06-18 15:50:52 -0300

    comentario

--- menu.js
+++ menu.js
@@ -2,3 +2,10 @@ import prompt from "prompt-sync";
 import {} from './app.js'
 let ler = prompt ()
 
+let v = exibirSaldo(saldoAtual)
+console.log(v)
+
+let t = sacar(saldoAtual ,saque)
+console.log(t)
+
+console.log(saldoAtual)
